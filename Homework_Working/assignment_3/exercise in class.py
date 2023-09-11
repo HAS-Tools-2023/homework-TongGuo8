729f933e2fd3e6b4498508be753339e0aba96e17
TongGuo8 <tongguo8@arizona.edu>
2023-09-12 02:02:54 +0800

    exercise in class1

---
+++ Homework_Working/assignment_3/exercise in class.py
@@ -0,0 +1,44 @@
+# %% 
+mylist = ["a","b","c","d"]
+# %%
+#mylist = ['a', 'b', 'c', 'd']
+# %%
+#mylist
+# %%
+print(mylist[0],mylist[1])
+# %%
+print(mylist[0], mylist[1])
+# %%
+x = 0
+for i in range(10):
+    x += 1
+    print('On number', i, 'x is now:', x)
+x
+
+# %%
+mylist = ['a', 'b', 'c', 'd']
+# %%
+print(mylist[0],mylist[1])
+# %%
+int(4.5)
+# %%
+int(9.9)
+# %%
+list_of_lists=[[1, 2, 3], [8, 9, 10]]
+list_of_lists
+# %%
+list_of_lists[0]
+# %%
+type(list_of_lists)
+# %%
+list_of_lists[0][1]
+# %%
+march_precip_in = 1.85
+in_to_mm = 25.4
+march_precip_mm = march_precip_in * in_to_mm
+print(march_precip_mm)
+# %%
+type(march_precip_mm)
+# %%
+type(march_precip_in)
+# %%
